14aa5285c5afc593464d91f85baf0d623f7114b3
Coral Sheldon-Hess <coral@sheldon-hess.org>
2013-12-25 23:15:09 -0900

    Gave it an HTML test harness; commented the code better; put my to-do list item in

--- accordion.js
+++ accordion.js
@@ -1,13 +1,18 @@
+
+// hide the divs in class "blocks"
 $(".blocks").hide();
-$('a').children('h2').append(' <img src="assets/images/buttons/arrow_closed.gif" class="arrows" name="click to show/hide text" />');
+
+// add arrows to the headings - ** SUB IN YOUR OWN FILE STRUCTURE (and other arrows, if you want) **
+$('a').children('h2').append(' <img src="images/arrow_closed.gif" class="arrows" name="click to show/hide text" />');
 var open_filename = "arrow_open.gif";
 var closed_filename = "arrow_closed.gif";
 
+// toggle the visibility of the text when the header's clicked; also flip the arrow
 $(".headings").click(function() { 
     var theblock =  $(this).next(".blocks");
-
     theblock.toggle(0);
 
+    // something I tried, so I could use toggle("fast") instead; didn't work; continued below
     //var timeoutID = window.setTimeout(toggle_arrows($(this), theblock), 600);
 
     if (theblock.is(":hidden")) {
@@ -18,11 +23,13 @@ $(".headings").click(function() {
         var open_html = $(this).html().replace(closed_filename, open_filename);
         $(this).html(open_html);
     }
-    
 });
 
+// figure out the URL's target anchor (if any), and make that div visible
 
 
+
+// trying to deal with the delay in toggle("fast")
 /*function toggle_arrows(that, thatblock) {
 	if (thatblock.is(":hidden")) {
     	var closed_html = that.html().replace(open_filename, closed_filename);
