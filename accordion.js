a0eafb251e62899e41057ae348d978e1b891f162
Coral Sheldon-Hess <coral@sheldon-hess.org>
2014-01-08 17:29:59 -0900

    Opens appropriate div when anchor included in URL
    The arrows don’t do what I want. That’s a bummer. BUMMER. I’ll try to
    fix that later tonight, or tomorrow.

--- accordion.js
+++ accordion.js
@@ -1,12 +1,26 @@
 
 // hide the divs in class "blocks"
 // hide the menu that doesn't show up if JavaScript's off, too
-$(".blocks, .anchors").hide();
+$('.blocks, .anchors').hide();
+
+
 
 // add arrows to the headings - ** SUB IN YOUR OWN FILE STRUCTURE (and other arrows, if you want) **
 $('a').children('h2').append(' <img src="images/arrow_closed.gif" class="arrows" name="click to show/hide text" />');
 var open_filename = "arrow_open.gif";
 var closed_filename = "arrow_closed.gif";
+
+// if going right to a sub-heading via anchor tag (http://la.la.com/la.html#anchor), open that section
+// **** IMPORTANT: in the HTML file, make sure you match names 
+// between every <a name=""> and its following <div class="blocks" id=""> ****
+var hash = window.location.hash; // gets the anchor tag from the URL
+$(hash).show(); // opens the div with the id of the anchor tag
+var theimg = $(hash).prev('a.headings').children('h2').children('img'); // gets the img BEFORE the div
+theimg.hide(); // this works
+
+// this doesn't:
+var arrow_down = theimg.html().replace(closed_filename, open_filename); 
+theimg.html(arrow_down); 
 
 // toggle the visibility of the text when the header's clicked; also flip the arrow
 $(".headings").click(function() { 
@@ -25,10 +39,6 @@ $(".headings").click(function() {
         $(this).html(open_html);
     }
 });
-
-// figure out the URL's target anchor (if any), and make that div visible
-
-
 
 // trying to deal with the delay in toggle("fast")
 /*function toggle_arrows(that, thatblock) {
