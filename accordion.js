7b1ba950d0faf7787081eb44200b29aea4febafd
Coral Sheldon-Hess <coral@sheldon-hess.org>
2014-01-09 20:13:59 -0900

    Fixed the arrow of the heading that goes with the #anchor
    And there’s a frog on the bump on the log in the hole in the bottom of
    the sea

--- accordion.js
+++ accordion.js
@@ -3,9 +3,7 @@
 // hide the menu that doesn't show up if JavaScript's off, too
 $('.blocks, .anchors').hide();
 
-
-
-// add arrows to the headings - ** SUB IN YOUR OWN FILE STRUCTURE (and other arrows, if you want) **
+// add arrows to the headings - ** SUB IN YOUR OWN FILE STRUCTURE HERE AND BELOW (and other arrows, if you want) **
 $('a').children('h2').append(' <img src="images/arrow_closed.gif" class="arrows" name="click to show/hide text" />');
 var open_filename = "arrow_open.gif";
 var closed_filename = "arrow_closed.gif";
@@ -16,19 +14,12 @@ var closed_filename = "arrow_closed.gif";
 var hash = window.location.hash; // gets the anchor tag from the URL
 $(hash).show(); // opens the div with the id of the anchor tag
 var theimg = $(hash).prev('a.headings').children('h2').children('img'); // gets the img BEFORE the div
-theimg.hide(); // this works
-
-// this doesn't:
-var arrow_down = theimg.html().replace(closed_filename, open_filename); 
-theimg.html(arrow_down); 
+theimg.prop('src','images/arrow_open.gif'); // **** sorry, have to sub in your file structure again
 
 // toggle the visibility of the text when the header's clicked; also flip the arrow
 $(".headings").click(function() { 
     var theblock =  $(this).next(".blocks");
     theblock.toggle(0);
-
-    // something I tried, so I could use toggle("fast") instead; didn't work; continued below
-    //var timeoutID = window.setTimeout(toggle_arrows($(this), theblock), 600);
 
     if (theblock.is(":hidden")) {
     	var closed_html = $(this).html().replace(open_filename, closed_filename);
@@ -39,15 +30,3 @@ $(".headings").click(function() {
         $(this).html(open_html);
     }
 });
-
-// trying to deal with the delay in toggle("fast")
-/*function toggle_arrows(that, thatblock) {
-	if (thatblock.is(":hidden")) {
-    	var closed_html = that.html().replace(open_filename, closed_filename);
-        that.html(closed_html);
-    }
-    else {
-        var open_html = that.html().replace(closed_filename, open_filename);
-        that.html(open_html);
-    }
-}*/
